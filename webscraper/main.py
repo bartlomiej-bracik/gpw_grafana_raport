0fee1102a26af37a369a17e095a07590036a98df
Bartek <bartlomiej.bracik@gmail.com>
2026-09-06 12:59:34 +0200

    Some company value visualisation in Grafana

--- webscraper/main.py
+++ webscraper/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+import requests
+from bs4 import BeautifulSoup
+import json
 
 app = FastAPI()
 
@@ -15,12 +18,41 @@ app.add_middleware(
 
 
 
-@app.get("/")
-def home():
-   
-    return {"message": "Test" }
-   
 
+
+@app.get("/orlen")
+def orlen():
+    url = "https://www.gpw.pl/spolka?isin=PLPKN0000018"
+    
+    
+    return {"Notowania Orlen": getCompanyValue(url) }
+
+@app.get("/pzu")
+def pzu():
+    url = "https://www.gpw.pl/spolka?isin=PLPZU0000011"
+    
+    
+    return {"Notowania PZU": getCompanyValue(url) }
+
+
+
+@app.get("/pko")
+def pko():
+    url = "https://www.gpw.pl/spolka?isin=PLPKO0000016"
+    
+    
+    return {"Notowania PKO": getCompanyValue(url) }
+
+
+
+def getCompanyValue(url):
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    value = soup.select_one(".summary").text.strip()
+    
+    return value
 
 if __name__ == "__main__":
     uvicorn.run(
